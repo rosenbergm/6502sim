c0237a3eaafc59604f7016beee56052ae8830e19
Martin Rosenberg <nitram.rosenberg@gmail.com>
2025-02-26 11:28:48 +0100

    fix byte utils carry/borrow

--- byte_utils.h
+++ byte_utils.h
@@ -35,6 +35,16 @@ static size_t hex_to_number(std::string &&hex) {
   return result;
 }
 
+constexpr bool is_negative(std::byte b) {
+  return (b & std::byte(1 << 7)) != ZERO_BYTE;
+}
+
+constexpr bool is_zero(std::byte b) { return b == ZERO_BYTE; }
+
+constexpr bool is_bit_set(std::byte b, int bit) {
+  return (b & std::byte(1 << bit)) != ZERO_BYTE;
+}
+
 struct byte_result {
   std::byte value{};
   bool carry;
@@ -45,68 +55,94 @@ struct byte_result {
       : value(value_), carry(carry_), negative(negative_) {}
 };
 
+// byte + byte
 constexpr byte_result operator+(std::byte a, std::byte b) {
   uint16_t result = std::to_integer<uint16_t>(a) + std::to_integer<uint16_t>(b);
-  return {std::byte(result & 0xFF), result > 0xFF, result > 0x7F};
+  std::byte byte_result = std::byte(result);
+
+  return {byte_result, result > 0xFF, is_negative(byte_result)};
 }
 
+// byte - byte
 constexpr byte_result operator-(std::byte a, std::byte b) {
+  bool borrow = a < b;
+
   uint16_t result = std::to_integer<uint16_t>(a) - std::to_integer<uint16_t>(b);
-  return {std::byte(result & 0xFF), result > 0xFF, result > 0x7F};
+  std::byte byte_result = std::byte(result);
+
+  return {byte_result, borrow, is_negative(byte_result)};
 }
 
-constexpr byte_result operator+(std::byte a, int b) {
+// byte + int
+constexpr byte_result operator+(std::byte a, uint8_t b) {
   uint16_t result = std::to_integer<uint16_t>(a) + b;
-  return {std::byte(result & 0xFF), result > 0xFF, result > 0x7F};
+  std::byte byte_result = std::byte(result);
+
+  return {byte_result, result > 0xFF, is_negative(byte_result)};
 }
 
-constexpr byte_result operator-(std::byte a, int b) {
+// byte - int
+constexpr byte_result operator-(std::byte a, uint8_t b) {
+  bool borrow = a < std::byte(b);
+
   uint16_t result = std::to_integer<uint16_t>(a) - b;
-  return {std::byte(result & 0xFF), result > 0xFF, result > 0x7F};
+  std::byte byte_result = std::byte(result);
+
+  return {byte_result, borrow, is_negative(byte_result)};
 }
 
-constexpr byte_result operator+(const byte_result &a, int b) {
+// byte_result + int
+constexpr byte_result operator+(const byte_result &a, uint8_t b) {
   uint16_t result = std::to_integer<uint16_t>(a.value) + b;
-  return {std::byte(result & 0xFF), result > 0xFF || a.carry, result > 0x7F};
+  std::byte byte_result = std::byte(result);
+
+  return {byte_result, result > 0xFF || a.carry, is_negative(byte_result)};
 }
 
-constexpr byte_result operator+(int b, const byte_result &a) {
-  uint16_t result = b + std::to_integer<uint16_t>(a.value);
-  return {std::byte(result & 0xFF), result > 0xFF || a.carry, result > 0x7F};
+// int + byte_result
+constexpr byte_result operator+(uint8_t b, const byte_result &a) {
+  uint16_t result = std::to_integer<uint16_t>(a.value) + b;
+  std::byte byte_result = std::byte(result);
+
+  return {byte_result, result > 0xFF || a.carry, is_negative(byte_result)};
 }
 
+// byte + byte_result
 constexpr byte_result operator+(std::byte a, const byte_result &b) {
   uint16_t result =
       std::to_integer<uint16_t>(a) + std::to_integer<uint16_t>(b.value);
-  return {std::byte(result & 0xFF), result > 0xFF || b.carry, result > 0x7F};
+  std::byte byte_result = std::byte(result);
+
+  return {byte_result, result > 0xFF || b.carry, is_negative(byte_result)};
 }
 
+// byte_result + byte
 constexpr byte_result operator+(const byte_result &a, std::byte b) {
   uint16_t result =
       std::to_integer<uint16_t>(a.value) + std::to_integer<uint16_t>(b);
-  return {std::byte(result & 0xFF), result > 0xFF || a.carry, result > 0x7F};
+  std::byte byte_result = std::byte(result);
+
+  return {byte_result, result > 0xFF || a.carry, is_negative(byte_result)};
 }
 
+// byte_result - byte
 constexpr byte_result operator-(const byte_result &a, std::byte b) {
+  bool borrow = a.value < b;
   uint16_t result =
       std::to_integer<uint16_t>(a.value) - std::to_integer<uint16_t>(b);
-  return {std::byte(result & 0xFF), result > 0xFF || a.carry, result > 0x7F};
+  std::byte byte_result = std::byte(result);
+
+  return {byte_result, borrow || a.carry, is_negative(byte_result)};
 }
 
+// byte - byte_result
 constexpr byte_result operator-(std::byte a, const byte_result &b) {
+  bool borrow = a < b.value;
   uint16_t result =
       std::to_integer<uint16_t>(a) - std::to_integer<uint16_t>(b.value);
-  return {std::byte(result & 0xFF), result > 0xFF || b.carry, result > 0x7F};
-}
+  std::byte byte_result = std::byte(result);
 
-constexpr bool is_negative(std::byte b) {
-  return (std::to_integer<int>(b) & 0x80);
-}
-
-constexpr bool is_zero(std::byte b) { return std::to_integer<int>(b) == 0; }
-
-constexpr bool is_bit_set(std::byte b, int bit) {
-  return (b & std::byte(1 << bit)) != ZERO_BYTE;
+  return {byte_result, borrow || b.carry, is_negative(byte_result)};
 }
 
 #endif
